6a12d7ee21a96adf474620b360f494a3d9a7e57e
limyujin12 <dladbwls95@gmail.com>
2021-10-12 20:52:23 +0900

    html 완료

--- components/Common/Testimonials.js
+++ components/Common/Testimonials.js
@@ -48,85 +48,110 @@
 
         render() {
             return (
-            <>
+              <>
                 <section className="feedback-area ptb-100 bg-color">
-                <div className="container">
+                  <div className="container">
                     <div className="section-title">
-                    <span className="sub-title">Review</span>
-                    <h2>고객 리뷰</h2>
+                      <span className="sub-title">Review</span>
+                      <h2>고객 리뷰</h2>
                     </div>
-                </div>
+                  </div>
 
-                {this.state.display ? (
+                  {this.state.display ? (
                     <OwlCarousel
-                    className="feedback-slides owl-carousel owl-theme"
-                    {...options}
+                      className="feedback-slides owl-carousel owl-theme"
+                      {...options}
                     >
-                    <div className="single-feedback-item">
+                      <div className="single-feedback-item">
                         <p>
-                                        “처음 사업을 시작 하면서 아무것도 모르고 시작했는데, YY AGENCY덕분에
-                                        너무 간편하게 홈페이지를 만들었어요ㅎㅎ 그리고 YY AGENCY 세미나를 통해
-                                        편하게 홈페이지를 유지 하고 있어 너무 감사드려요 세미나도 꼭 참석하려고요”
+                          “처음 사업을 시작 하면서 아무것도 모르고 시작했는데,
+                          YY AGENCY덕분에 너무 간편하게 홈페이지를
+                          만들었어요ㅎㅎ 그리고 YY AGENCY 세미나를 통해 편하게
+                          홈페이지를 유지 하고 있어 너무 감사드려요 세미나도 꼭
+                          참석하려고요”
                         </p>
                         <div className="client-info">
-                        <img src="/images/client-image/client1.png" alt="image" />
-                        <h3>김XX원장 고객님</h3>
+                          <img
+                            src="/images/client-image/client1.png"
+                            alt="image"
+                          />
+                          <h3>rlaXXXX 고객님</h3>
                         </div>
-                    </div>
+                      </div>
 
-                    <div className="single-feedback-item">
+                      <div className="single-feedback-item">
                         <p>
-                                        “온라인 사업으로 확장을 할 수 있게 도와준 YY AGENCY 너무 감사합니다.^^
-                                        덕분에 매출도 너무 많이 오르고 힘든 코로나 시기를 잘 이겨낼 수 있었어요.
-                                        YY AGENCY 직원 분들도 너무 친절하세요.”
+                          “온라인 사업으로 확장을 할 수 있게 도와준 YY AGENCY
+                          너무 감사합니다.^^ 덕분에 매출도 너무 많이 오르고 힘든
+                          코로나 시기를 잘 이겨낼 수 있었어요. YY AGENCY 직원
+                          분들도 너무 친절하세요.”
                         </p>
                         <div className="client-info">
-                        <img src="/images/client-image/client2.png" alt="image" />
-                        <h3>이XX 고객님</h3>
+                          <img
+                            src="/images/client-image/client2.png"
+                            alt="image"
+                          />
+                          <h3>이XX 고객님</h3>
                         </div>
-                    </div>
+                      </div>
 
-                    <div className="single-feedback-item">
+                      <div className="single-feedback-item">
                         <p>
-                                        "반응형 홈페이지를 너무 좋은 퀄리티로 만들어 주셔서 너무 감사해요. 많은 곳을 알아 봤지만
-                                        홈페이지 유지 관리를 이렇게 철저하게 알려 주시는 곳이 여기가 처음인거 같아요. 세미나 가면
-                                        또 많은 대표님들과 교류의 장을 열어 주셔서 너무 감사드려요”
+                          "반응형 홈페이지를 너무 좋은 퀄리티로 만들어 주셔서
+                          너무 감사해요. 많은 곳을 알아 봤지만 홈페이지 유지
+                          관리를 이렇게 철저하게 알려 주시는 곳이 여기가
+                          처음인거 같아요. 세미나 가면 또 많은 대표님들과 교류의
+                          장을 열어 주셔서 너무 감사드려요”
                         </p>
                         <div className="client-info">
-                        <img src="/images/client-image/client3.png" alt="image" />
-                        <h3>김XX 고객님</h3>
+                          <img
+                            src="/images/client-image/client3.png"
+                            alt="image"
+                          />
+                          <h3>김XX 고객님</h3>
                         </div>
-                    </div>
+                      </div>
 
-                    <div className="single-feedback-item">
+                      <div className="single-feedback-item">
                         <p>
-                                        “ 앞서가는 기술 정말 맞네요. 홈페이지 너무 잘 돌아가요 너무 감사합니다. 위트 있는 직원분들도 너무 좋네요
-                                        덕분에 좋은 시너지와 홈페이지 만들었어요 앞으로도 YY AGENCY와 함께 가고 싶어 회원사 가입했어요”
+                          “ 앞서가는 기술 정말 맞네요. 홈페이지 너무 잘 돌아가요
+                          너무 감사합니다. 위트 있는 직원분들도 너무 좋네요
+                          덕분에 좋은 시너지와 홈페이지 만들었어요 앞으로도 YY
+                          AGENCY와 함께 가고 싶어 회원사 가입했어요”
                         </p>
                         <div className="client-info">
-                        <img src="/images/client-image/client4.png" alt="image" />
-                        <h3>황XX 고객님</h3>
+                          <img
+                            src="/images/client-image/client4.png"
+                            alt="image"
+                          />
+                          <h3>황XX 고객님</h3>
                         </div>
-                    </div>
+                      </div>
 
-                    <div className="single-feedback-item">
+                      <div className="single-feedback-item">
                         <p>
-                                        "홈페이지 진짜 잘 만들어 주시네요 너무 편하게 가게를 운영 할 수있게 되어서 너무 행복합니다. 요즘 코시국 덕분에 걱정 진짜 많이
-                                        하고 가게 홈페이지 제작 했는데 저희 한테 너무 필요한 일이였네요 또한 새로운 사업파트너가 생긴거 같아 너무 좋아요
-                                        혹시 온라인 사업 확장 고민하시는 분들 꼭 YY AGENCY에서 하세요 정말 많은 도움이 되었습니다.”
+                          "홈페이지 진짜 잘 만들어 주시네요 너무 편하게 가게를
+                          운영 할 수있게 되어서 너무 행복합니다. 요즘 코시국
+                          덕분에 걱정 진짜 많이 하고 가게 홈페이지 제작 했는데
+                          저희 한테 너무 필요한 일이였네요 또한 새로운
+                          사업파트너가 생긴거 같아 너무 좋아요 혹시 온라인 사업
+                          확장 고민하시는 분들 꼭 YY AGENCY에서 하세요 정말 많은
+                          도움이 되었습니다.”
                         </p>
                         <div className="client-info">
-                        <img src="/images/client-image/client5.png" alt="image" />
-                        <h3>살롱하는새댁 고객님</h3>
-                        
+                          <img
+                            src="/images/client-image/client5.png"
+                            alt="image"
+                          />
+                          <h3>dlekXXX고객님</h3>
                         </div>
-                    </div>
+                      </div>
                     </OwlCarousel>
-                ) : (
+                  ) : (
                     ""
-                )}
+                  )}
                 </section>
-            </>
+              </>
             );
         }
     }
--- components/ServicesThree/Services.js
+++ components/ServicesThree/Services.js
@@ -4,222 +4,234 @@
     class Services extends Component {
         render() {
             return (
-            <section className="ptb-100">
+              <section className="ptb-100">
                 <div className="container">
-                <div className="row">
+                  <div className="row">
                     <div className="col-lg-4 col-sm-6">
-                    <div className="service-card-one gray-bg">
+                      <div className="service-card-one gray-bg">
                         <div className="icon">
-                        <i className="bx bx-conversation"></i>
-                        <i className="bx bxs-badge-check check-icon"></i>
+                          <i className="bx bx-mobile-alt"></i>
+                          <i className="bx bxs-badge-check check-icon"></i>
                         </div>
                         <h3>
-                        <Link href="/service-details">
+                          <Link href="/service-details">
                             <a>프리미엄 반응형 웹사이트</a>
-                        </Link>
+                          </Link>
                         </h3>
                         <p>
-                        스마트한 시대에 맞춰 프리미엄 반응형 웹사이트는 기본입니다. 모바일 PC 어디서든 최적화된 사이트를 제공합니다.
+                          스마트한 시대에 맞춰 프리미엄 반응형 웹사이트는
+                          기본입니다. 모바일 PC 어디서든 최적화된 사이트를
+                          제공합니다.
                         </p>
-                    </div>
+                      </div>
                     </div>
                     <div className="col-lg-4 col-sm-6">
-                    <div className="service-card-one gray-bg">
+                      <div className="service-card-one gray-bg">
                         <div className="icon">
-                        <i className="bx bx-conversation"></i>
-                        <i className="bx bxs-badge-check check-icon"></i>
+                          <i className="bx bx-purchase-tag-alt"></i>
+                          <i className="bx bxs-badge-check check-icon"></i>
                         </div>
                         <h3>
-                        <Link href="/service-details">
+                          <Link href="/service-details">
                             <a>수익&amp;브랜딩 홈페이지 제작</a>
-                        </Link>
+                          </Link>
                         </h3>
                         <p>
-                        반응형 웹사이트를 넘어 수익과 브랜딩을 함께 <br/>진행 할 수 있는 새로운 홈페이지입니다.
+                          반응형 웹사이트를 넘어 수익과 브랜딩을 함께 <br />
+                          진행 할 수 있는 새로운 홈페이지입니다.
                         </p>
-                    </div>
+                      </div>
                     </div>
                     <div className="col-lg-4 col-sm-6">
-                    <div className="service-card-one gray-bg">
+                      <div className="service-card-one gray-bg">
                         <div className="icon">
-                        <i className="bx bx-conversation"></i>
-                        <i className="bx bxs-badge-check check-icon"></i>
+                          <i className="bx bxs-download"></i>
+                          <i className="bx bxs-badge-check check-icon"></i>
                         </div>
                         <h3>
-                        <Link href="/service-details">
+                          <Link href="/service-details">
                             <a>SSL 설치</a>
-                        </Link>
+                          </Link>
                         </h3>
                         <p>
-                        SSL은 홈페이지의 보안을 위한 인증서로써 홈페이지 보안을
-                        위한 필수 요소입니다
+                          SSL은 홈페이지의 보안을 위한 인증서로써 홈페이지
+                          보안을 위한 필수 요소입니다
                         </p>
-                    </div>
+                      </div>
                     </div>
 
                     <div className="col-lg-4 col-sm-6">
-                    <div className="service-card-one gray-bg">
+                      <div className="service-card-one gray-bg">
                         <div className="icon">
-                        <i className="bx bx-search-alt"></i>
-                        <i className="bx bxs-badge-check check-icon"></i>
+                          <i className="bx bx-like"></i>
+                          <i className="bx bxs-badge-check check-icon"></i>
                         </div>
                         <h3>
-                        <Link href="/service-details">
+                          <Link href="/service-details">
                             <a>SEO최적화</a>
-                        </Link>
+                          </Link>
                         </h3>
                         <p>SEO 최적화로 등록을 도와드립니다.</p>
-                    </div>
+                      </div>
                     </div>
 
                     <div className="col-lg-4 col-sm-6">
-                    <div className="service-card-one gray-bg">
+                      <div className="service-card-one gray-bg">
                         <div className="icon">
-                        <i className="bx bx-bullseye"></i>
-                        <i className="bx bxs-badge-check check-icon"></i>
+                          <i className="bx bx-link"></i>
+                          <i className="bx bxs-badge-check check-icon"></i>
                         </div>
                         <h3>
-                        <Link href="/service-details">
+                          <Link href="/service-details">
                             <a>포털사이트 사이트 등록 및 파워링크 설정</a>
-                        </Link>
+                          </Link>
                         </h3>
                         <p>
-                        네이버 다음 구글에 사이트 등록및 파워링크 세팅을
-                        도와드립니다.
+                          네이버 다음 구글에 사이트 등록및 파워링크 세팅을
+                          도와드립니다.
                         </p>
-                    </div>
+                      </div>
                     </div>
 
                     <div className="col-lg-4 col-sm-6">
-                    <div className="service-card-one gray-bg">
+                      <div className="service-card-one gray-bg">
                         <div className="icon">
-                        <i className="bx bx-bar-chart-alt-2"></i>
-                        <i className="bx bxs-badge-check check-icon"></i>
+                          <i className="bx bxs-keyboard"></i>
+                          <i className="bx bxs-badge-check check-icon"></i>
                         </div>
                         <h3>
-                        <Link href="/service-details">
+                          <Link href="/service-details">
                             <a>로얄 키워드 등록</a>
-                        </Link>
+                          </Link>
                         </h3>
                         <p>
-                        좋은 키워드를 넣어야 더 좋은 광고 효과를 낼 수 있습니다. 따라서 로얄키워드 등록을 도와드립니다.
+                          좋은 키워드를 넣어야 더 좋은 광고 효과를 낼 수
+                          있습니다. 따라서 로얄키워드 등록을 도와드립니다.
                         </p>
-                    </div>
+                      </div>
                     </div>
 
                     <div className="col-lg-4 col-sm-6">
-                    <div className="service-card-one gray-bg">
+                      <div className="service-card-one gray-bg">
                         <div className="icon">
-                        <i className="bx bx-check-shield"></i>
-                        <i className="bx bxs-badge-check check-icon"></i>
+                          <i className="bx bx-check-shield"></i>
+                          <i className="bx bxs-badge-check check-icon"></i>
                         </div>
                         <h3>
-                        <Link href="/service-details">
+                          <Link href="/service-details">
                             <a>안전한 간편로그인 </a>
-                        </Link>
+                          </Link>
                         </h3>
                         <p>
-                        카카오톡 네이버 아이디 구글 간편로그인으로 새로운 회원을 모집해보세요.
+                          카카오톡 네이버 아이디 구글 간편로그인으로 새로운
+                          회원을 모집해보세요.
                         </p>
-                    </div>
+                      </div>
                     </div>
 
                     <div className="col-lg-4 col-sm-6">
-                    <div className="service-card-one gray-bg">
+                      <div className="service-card-one gray-bg">
                         <div className="icon">
-                        <i className="bx bx-bar-chart-alt-2"></i>
-                        <i className="bx bxs-badge-check check-icon"></i>
+                          <i className="bx bx-credit-card"></i>
+                          <i className="bx bxs-badge-check check-icon"></i>
                         </div>
                         <h3>
-                        <Link href="/service-details">
+                          <Link href="/service-details">
                             <a>안전한 결제 시스템 설치</a>
-                        </Link>
+                          </Link>
                         </h3>
                         <p>
-                        쇼핑몰 또는 결제 시스템이 필요한 모든 업종의 안전한 결제 시스템을 설치해드립니다.
+                          쇼핑몰 또는 결제 시스템이 필요한 모든 업종의 안전한
+                          결제 시스템을 설치해드립니다.
                         </p>
-                    </div>
+                      </div>
                     </div>
 
                     <div className="col-lg-4 col-sm-6">
-                    <div className="service-card-one gray-bg">
+                      <div className="service-card-one gray-bg">
                         <div className="icon">
-                        <i className="bx bx-search-alt"></i>
-                        <i className="bx bxs-badge-check check-icon"></i>
+                          <i className="bx bx-list-plus"></i>
+                          <i className="bx bxs-badge-check check-icon"></i>
                         </div>
                         <h3>
-                        <Link href="/service-details">
+                          <Link href="/service-details">
                             <a>유지보수 교육 시스템</a>
-                        </Link>
+                          </Link>
                         </h3>
                         <p>
-                        홈페이지 유지 및 보수를 위한 교육 및 세미나를 진행하여 고객 여러분들의 어려움을 줄여드립니다.
+                          홈페이지 유지 및 보수를 위한 교육 및 세미나를 진행하여
+                          고객 여러분들의 어려움을 줄여드립니다.
                         </p>
-                    </div>
+                      </div>
                     </div>
 
                     <div className="col-lg-4 col-sm-6">
-                    <div className="service-card-one gray-bg">
+                      <div className="service-card-one gray-bg">
                         <div className="icon">
-                        <i className="bx bx-mail-send"></i>
-                        <i className="bx bxs-badge-check check-icon"></i>
+                          <i className="bx bx-news"></i>
+                          <i className="bx bxs-badge-check check-icon"></i>
                         </div>
                         <h3>
-                        <Link href="/service-details">
+                          <Link href="/service-details">
                             <a>퍼포먼스 마케팅 교육</a>
-                        </Link>
+                          </Link>
                         </h3>
-                                    <p>
-                                        사업에서 가장 막막한 마케팅 저희가 도와드립니다.동영상 편집, 인스타그램 마케팅,블로그 마케팅등의 퍼포먼스 마케팅 교육및 세미나를 진행합니다.
+                        <p>
+                          사업에서 가장 막막한 마케팅 저희가 도와드립니다.동영상
+                          편집, 인스타그램 마케팅,블로그 마케팅등의 퍼포먼스
+                          마케팅 교육및 세미나를 진행합니다.
                         </p>
-                    </div>
+                      </div>
                     </div>
 
                     <div className="col-lg-4 col-sm-6">
-                    <div className="service-card-one gray-bg">
+                      <div className="service-card-one gray-bg">
                         <div className="icon">
-                        <i className="bx bx-data"></i>
-                        <i className="bx bxs-badge-check check-icon"></i>
+                          <i className="bx bx-group"></i>
+                          <i className="bx bxs-badge-check check-icon"></i>
                         </div>
                         <h3>
-                        <Link href="/service-details">
+                          <Link href="/service-details">
                             <a>회원사간의 교류의장</a>
-                        </Link>
+                          </Link>
                         </h3>
                         <p>
-                        혼자서 혹은 작은 인원으로 시작하시는 대표님들 걱정하지마세요. 저희 회원사 대표님들의 교류를 위한 만남의 장을 열겠습니다. 정기적으로 교류 하며 상생효과를 누려 보세요.
+                          혼자서 혹은 작은 인원으로 시작하시는 대표님들
+                          걱정하지마세요. 저희 회원사 대표님들의 교류를 위한
+                          만남의 장을 열겠습니다. 정기적으로 교류 하며
+                          상생효과를 누려 보세요.
                         </p>
-                    </div>
+                      </div>
                     </div>
 
                     {/* Pagination */}
-                    <div className="col-lg-12 col-sm-12">
-                    <div className="pagination-area">
+                    {/* <div className="col-lg-12 col-sm-12">
+                      <div className="pagination-area">
                         <Link href="/services3#">
-                        <a className="prev page-numbers">
+                          <a className="prev page-numbers">
                             <i className="fas fa-angle-double-left"></i>
-                        </a>
+                          </a>
                         </Link>
                         <Link href="/services3#">
-                        <a className="page-numbers">1</a>
+                          <a className="page-numbers">1</a>
                         </Link>
                         <span className="page-numbers current">2</span>
                         <Link href="/services3#">
-                        <a className="page-numbers">3</a>
+                          <a className="page-numbers">3</a>
                         </Link>
                         <Link href="/services3#">
-                        <a className="page-numbers">4</a>
+                          <a className="page-numbers">4</a>
                         </Link>
                         <Link href="/services3#">
-                        <a className="next page-numbers">
+                          <a className="next page-numbers">
                             <i className="fas fa-angle-double-right"></i>
-                        </a>
+                          </a>
                         </Link>
-                    </div>
-                    </div>
+                      </div>
+                    </div> */}
+                  </div>
                 </div>
-                </div>
-            </section>
+              </section>
             );
         }
     }
