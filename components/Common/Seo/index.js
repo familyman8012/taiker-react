1102883b64312826964589e01e6b3a39ef465449
limyujin12 <dladbwls95@gmail.com>
2021-10-13 19:30:04 +0900

    seo 사진 수정 1

--- components/Common/Seo/index.js
+++ components/Common/Seo/index.js
@@ -59,7 +59,7 @@ export function IndexSeo() {
       url: "https://www.yyagency.co.kr/",
       images: [
         {
-          url: "https://www.yyagency.co.kr//images/seo-bannertwi001t2689493_l (1).jpg",
+          url: "https://www.yyagency.co.kr//images/seo-banner/twi001t2689493_l.jpg",
           width: 1200,
           height: 630,
           alt: "홈페이지 제작 에이전시 YY AGENCY 홍보이미지",
@@ -85,7 +85,7 @@ export function AboutSeo() {
       url: "https://www.yyagency.co.kr//about",
       images: [
         {
-          url: "https://www.yyagency.co.kr//images/seo-banner/tip014t012590.jpg",
+          url: "https://www.yyagency.co.kr//images/seo-banner/seoimg.jpg",
           width: 1200,
           height: 630,
           alt: "홈페이지 제작 에이전시 YY AGENCY 소개 홍보이미지",
@@ -105,12 +105,13 @@ export function ServicesSeo() {
       "반응형 웹페이지,SEO, SSL,Google AD, Naver파워링크 세팅, 로얄키워드 등록, 포털사이트 사이트 등록, 마케팅 &amp; 홈페이지 유지 보수 관리 교육, 고객님들의 인적 네트워크 제공을 합니다 .",
     openGraph: {
       type: "website",
-      title: "비지니스 확장 혹은 새로운 비지니스를 하고 싶다면? - YY AGENCY 예약",
+      title:
+        "비지니스 확장 혹은 새로운 비지니스를 하고 싶다면? - YY AGENCY 예약",
       description: `저희는 여러분의 온라인 비지니스를 저희의 일처럼 성실하게 좋은 퀄리티로 만들어 드리는 파트너입니다.`,
       url: "https://www.yyagency.co.kr//services",
       images: [
         {
-          url: "https://www.yyagency.co.kr//images/seo-banner/tip114t002503.jpg",
+          url: "https://www.yyagency.co.kr//images/seo-banner/YYAGENCY_HOMEPAGE.png",
           width: 1200,
           height: 630,
           alt: "홈페이지 제작 에이전시 YY AGENCY 소개 홍보이미지",
@@ -125,9 +126,9 @@ export function ServicesSeo() {
 export function PortfolioSeo() {
   const CounsellingSeoData = {
     canonical: "https://www.yyagency.co.kr//portfolio",
-    title: "철저한 맞춤 심리상담을 위한 상담신청작성 - YY AGENCY",
+    title: "철저한 회의를 통한 명품 반응형 웹사이트 - YY AGENCY",
     description:
-      "YY AGENCY는 내담자 한 분, 한 분의 이야기를 주의깊게 듣고 철저한 맞춤형 상담이 이루어지고 있습니다. 상당 전 신청내용을 바탕으로 저희만의 MCI 진단을 통해 상담에 들어가게 됩니다.",
+      "YY AGENCY는 다양한 홈페이지 제작을 경험으로 토대로 맞춤 홈페이지 제작을 진행합니다.",
 
     openGraph: {
       type: "website",
