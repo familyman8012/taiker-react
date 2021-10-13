6050a6162d9db052317ae58b5e7ff38ae9da880f
limyujin12 <dladbwls95@gmail.com>
2021-10-13 20:07:55 +0900

    SEO link

--- pages/projects.js
+++ pages/projects.js
@@ -3,13 +3,13 @@
     import PageHeader from '../components/Common/PageHeader';
     import ProjectsCard from '../components/Projects/ProjectsCard';
     import Footer from '../components/Layout/Footer';
-
+import PortfolioSeo from '../components/Common/Seo'
     class Projects extends Component {
         render() {
             return (
             <>
                 <Navbar />
-
+<PortfolioSeo/>
                 <PageHeader
                 pageTitle="YY AGENCY 포토폴리오"
                 breadcrumbTextOne="Home"
