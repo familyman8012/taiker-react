8cd520a7fe928e567a9d792ac1fa428678dbe195
limyujin12 <dladbwls95@gmail.com>
2021-10-23 19:17:14 +0900

    인스타 변경완료

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -5,121 +5,183 @@ class Footer extends Component {
     render() {
         let currentYear = new Date().getFullYear();
         return (
-            <>
-                <section className="footer-area">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-lg-4 col-md-6 col-sm-6">
-                                <div className="single-footer-widget">
-                                    <h3>Contact Info</h3>
+          <>
+            <section className="footer-area">
+              <div className="container">
+                <div className="row">
+                  <div className="col-lg-4 col-md-6 col-sm-6">
+                    <div className="single-footer-widget">
+                      <h3>제작 문의 및 SNS</h3>
 
-                                    <ul className="footer-contact-info">
-                                        <li>
-                                            <i className="flaticon-phone-call"></i>
-                                            <span>Mon to Fri : 10:00AM - 06:00PM</span>
-                                           
-                                            <a href="tel:+8201071452679">010-7145-2679</a>
-                                        </li>
-                                        <li>
-                                            <i className="flaticon-email"></i>
-                                            <span>Do You Have a Question?</span>
-                                            <a href="mailto:example@taiker.com">yyagency7@gmail.com</a>
-                                        </li>
-                                        <li>
-                                            <i className="flaticon-social-media"></i>
-                                            <span>Socials Network</span>
+                      <ul className="footer-contact-info">
+                        <li>
+                          <i className="flaticon-phone-call"></i>
+                          <span>월~토 : 10:00AM - 06:00PM</span>
 
-                                            <ul className="social">
-                                                <li>
-                                                    <a href="https://twitter.com/" target="_blank"><i className="fab fa-twitter"></i></a>
-                                                </li>
-                                                <li>
-                                                    <a href="https://www.facebook.com/" target="_blank"><i className="fab fa-facebook-f"></i></a>
-                                                </li>
-                                                <li>
-                                                    <a href="https://www.instagram.com/" target="_blank"><i className="fab fa-instagram"></i></a>
-                                                </li>
-                                                <li>
-                                                    <a href="https://www.linkedin.com/" target="_blank"><i className="fab fa-linkedin"></i></a>
-                                                </li>
-                                                <li>
-                                                    <a href="https://www.youtube.com/" target="_blank"><i className="fab fa-youtube"></i></a>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
+                          <a href="tel:+8201071452679">010-7145-2679</a>
+                        </li>
+                        <li>
+                          <i className="flaticon-email"></i>
+                          <span>이메일 문의하기</span>
+                          <a href="mailto:example@taiker.com">
+                            yyagency7@gmail.com
+                          </a>
+                        </li>
+                        <li>
+                          <i className="flaticon-social-media"></i>
+                          <span>SNS</span>
 
-                            
-                            <div className="col-lg-4 col-md-6 col-sm-6 offset-lg-0 offset-md-3 offset-sm-3">
-                                <div className="single-footer-widget pl-5">
-                                    <h3>Instagram</h3>
-
-                                    <ul className="footer-instagram-post">
-                                        <li>
-                                            <a href="https://www.instagram.com/" target="_blank">
-                                                <img src="/images/instagram-image/insta-img1.jpg" alt="image" />
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="https://www.instagram.com/" target="_blank">
-                                                <img src="/images/instagram-image/insta-img2.jpg" alt="image" />
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="https://www.instagram.com/" target="_blank">
-                                                <img src="/images/instagram-image/insta-img3.jpg" alt="image" />
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="https://www.instagram.com/" target="_blank">
-                                                <img src="/images/instagram-image/insta-img4.jpg" alt="image" />
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="https://www.instagram.com/" target="_blank">
-                                                <img src="/images/instagram-image/insta-img5.jpg" alt="image" />
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="https://www.instagram.com/" target="_blank">
-                                                <img src="/images/instagram-image/insta-img6.jpg" alt="image" />
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="copyright-area">
-                            <div className="row align-items-center">
-                                <div className="col-lg-6 col-sm-6 col-md-6">
-                                    <p>
-                                        Copyright &copy;{currentYear} YY Agency. 
-                                        All rights reserved <a href="https://envytheme.com/" target="_blank">YY Agnecy</a>
-                                    </p>
-                                </div>
-
-                                <div className="col-lg-6 col-sm-6 col-md-6">
-                                    <ul>
-                                        <li>
-                                            <Link href="/terms-and-conditions">
-                                                <a>Terms &amp; Conditions</a>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href="/privacy-policy">
-                                                <a>Privacy Policy</a>
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
+                          <ul className="social">
+                            <li>
+                              <a href="https://twitter.com/" target="_blank">
+                                <i className="fab fa-twitter"></i>
+                              </a>
+                            </li>
+                            <li>
+                              <a
+                                href="https://www.facebook.com/"
+                                target="_blank"
+                              >
+                                <i className="fab fa-facebook-f"></i>
+                              </a>
+                            </li>
+                            <li>
+                              <a
+                                href="https://www.instagram.com/yyagency_webpage/"
+                                target="_blank"
+                              >
+                                <i className="fab fa-instagram"></i>
+                              </a>
+                            </li>
+                            <li>
+                              <a
+                                href="https://www.linkedin.com/"
+                                target="_blank"
+                              >
+                                <i className="fab fa-linkedin"></i>
+                              </a>
+                            </li>
+                            <li>
+                              <a
+                                href="https://www.youtube.com/"
+                                target="_blank"
+                              >
+                                <i className="fab fa-youtube"></i>
+                              </a>
+                            </li>
+                          </ul>
+                        </li>
+                      </ul>
                     </div>
-                </section>
-            </>
+                  </div>
+
+                  <div className="col-lg-4 col-md-6 col-sm-6 offset-lg-0 offset-md-3 offset-sm-3">
+                    <div className="single-footer-widget pl-5">
+                      <h3>Instagram</h3>
+
+                      <ul className="footer-instagram-post">
+                        <li>
+                          <a
+                            href="https://www.instagram.com/yyagency_webpage/"
+                            target="_blank"
+                          >
+                            <img
+                              src="/images/instagram-image/insta-img1.jpg"
+                              alt="image"
+                            />
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="https://www.instagram.com/yyagency_webpage/"
+                            target="_blank"
+                          >
+                            <img
+                              src="/images/instagram-image/insta-img2.png"
+                              alt="image"
+                            />
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="https://www.instagram.com/yyagency_webpage/"
+                            target="_blank"
+                          >
+                            <img
+                              src="/images/instagram-image/insta-img3.png"
+                              alt="image"
+                            />
+                          </a>
+                        </li>
+                        {/* <li>
+                          <a
+                            href="https://www.instagram.com/yyagency_webpage/"
+                            target="_blank"
+                          >
+                            <img
+                              src="/images/instagram-image/insta-img4.jpg"
+                              alt="image"
+                            />
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="https://www.instagram.com/yyagency_webpage/"
+                            target="_blank"
+                          >
+                            <img
+                              src="/images/instagram-image/insta-img5.jpg"
+                              alt="image"
+                            />
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="https://www.instagram.com/yyagency_webpage/"
+                            target="_blank"
+                          >
+                            <img
+                              src="/images/instagram-image/insta-img6.jpg"
+                              alt="image"
+                            />
+                          </a>
+                        </li> */}
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="copyright-area">
+                  <div className="row align-items-center">
+                    <div className="col-lg-6 col-sm-6 col-md-6">
+                      <p>
+                        Copyright &copy;{currentYear} YY Agency. All rights
+                        reserved{" "}
+                        <a href="https://envytheme.com/" target="_blank">
+                          YY Agnecy
+                        </a>
+                      </p>
+                    </div>
+
+                    <div className="col-lg-6 col-sm-6 col-md-6">
+                      <ul>
+                        <li>
+                          <Link href="/terms-and-conditions">
+                            <a>Terms &amp; Conditions</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/privacy-policy">
+                            <a>Privacy Policy</a>
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </>
         );
     }
 }
