5053668bc59e950296d459e9715c8af0d4f55f71
familyman8012 <familyman80@naver.com>
2021-10-31 20:58:10 +0900

    태그매니저로 교체

--- pages/_document.js
+++ pages/_document.js
@@ -1,26 +1,21 @@
-    import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-    import { GA_TRACKING_ID } from "../utils/gtag"
+// import { GA_TRACKING_ID } from "../utils/gtag"
 
-    class MyDocument extends Document {
-        
-        static async getInitialProps(ctx) {
-            const initialProps = await Document.getInitialProps(ctx)
-            return { ...initialProps }
-        }
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
 
-        render() {
-            return (
-            <Html lang="zxx">
-                <Head>
-                <link
-                    rel="icon"
-                    type="image/png"
-                    href="/images/favicon.png"
-                ></link>
-                <script defer src="js/channeltalk.js"></script>
-                {/* Global Site Tag (gtag.js) - Google Analytics */}
-                <script
+  render() {
+    return (
+      <Html lang="zxx">
+        <Head>
+          <link rel="icon" type="image/png" href="/images/favicon.png"></link>
+          <script defer src="js/channeltalk.js"></script>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          {/* <script
                     async
                     src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
                 />
@@ -36,15 +31,15 @@
                 });
             `,
                     }}
-                />
-                </Head>
-                <body>
-                <Main />
-                <NextScript />
-                </body>
-            </Html>
-            );
-        }
-    }
+                /> */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
 
-    export default MyDocument;
+export default MyDocument;
