eb408cc297084b69a1848c840fdc5e4aad01889f
familyman <familyman801205@gmai.com>
2021-11-03 22:25:14 +0900

    구글광고 + 구글애널리스틱

--- utils/gtag.js
+++ utils/gtag.js
@@ -1,12 +1,13 @@
 // lib/gtag.js
 
-export const GA_TRACKING_ID = "AW-10800891941";
+export const GA_TRACKING_ID = "UA-210111744-1";
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
   window.gtag("config", GA_TRACKING_ID, {
     page_path: url,
   });
+  gtag("config", "AW-10800891941");
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
